4215a3035a4937a34b480cf8611eb822d3bde24d
Praise Jacob <iamfelixkyun@gmail.com>
2025-08-01 19:25:16 +0530

    initial commit

---
+++ poc.ts
@@ -0,0 +1,50 @@
+const searchQuery = `
+    query( $search: SearchInput
+           $limit: Int
+           $page: Int
+           $translationType: VaildTranslationTypeEnumType
+           $countryOrigin: VaildCountryOriginEnumType )
+    {
+        shows( search: $search
+                limit: $limit
+                page: $page
+                translationType: $translationType
+                countryOrigin: $countryOrigin )
+        {
+            edges 
+            {
+                _id,
+                name,
+                availableEpisodes
+            }
+        }
+    }
+`;
+const apiUrl = "https://api.allanime.day/api";
+
+const variables = {
+  search: {
+    query: "One Piece",
+  },
+  limit: 10,
+  page: 1,
+  // translationType: "SUB",
+  countryOrigin: "ALL",
+};
+
+const params = new URLSearchParams({
+  variables: JSON.stringify(variables),
+  query: searchQuery,
+}).toString();
+
+console.log(params);
+
+fetch(`${apiUrl}?${params}`, {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+    Referer: "https://allmanga.to/",
+  },
+})
+  .then((res) => res.json())
+  .then((data) => console.log(data.data.shows.edges));
