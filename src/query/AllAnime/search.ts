e9b446d3b36f548128a2f518ca9f85d76e358594
Praise Jacob <iamfelixkyun@gmail.com>
2025-08-02 19:28:53 +0530

    refactor(allanime): add gql schema for search query

--- src/query/AllAnime/search.ts
+++ src/query/AllAnime/search.ts
@@ -23,3 +23,88 @@ query (
     }
 }
 `;
+
+/*
+ * extracted schema
+shows(
+    search: SearchInput
+    page: Int
+    offset: Int
+    limit: Int
+    translationType: VaildTranslationTypeEnumType
+    countryOrigin: VaildCountryOriginEnumType
+    queryAt: String
+): ShowsConnection!
+
+type ShowsConnection {
+  edges: [Show!]
+  pageInfo: PageInfo!
+}
+
+type Show {
+  _id: String
+  updateQueue: BigInt
+  isAdult: Boolean
+  manualUpdated: Boolean
+  dailyUpdateNeeded: Boolean
+  hidden: Boolean
+  lastUpdateStart: DateTime
+  lastUpdateEnd: DateTime
+  name: String
+  englishName: String
+  nativeName: String
+  nameOnlyString: String
+  countryOfOrigin: VaildCountryOriginEnumType
+  malId: BigInt
+  aniListId: BigInt
+  status: String
+  altNames: [String]
+  trustedAltNames: [String]
+  description: String
+  prevideos: [String]
+  thumbnail: String
+  banner: String
+  thumbnails: [String]
+  musics: [Object]
+  score: Float
+  type: String
+  averageScore: Float
+  genres: [String]
+  tags: [String]
+  popularity: BigInt
+  airedStart: Object
+  airedEnd: Object
+  season: Object
+  rating: String
+  broadcastInterval: BigInt
+  relatedShows: [Object]
+  relatedMangas: [Object]
+  characters: [Object]
+  pageStatus: PageStatus
+  determinedInterval: Object
+  slugTime: BigInt
+  episodeCount: BigInt
+  episodeDuration: BigInt
+  studios: [String]
+  nextAiringEpisode: Object
+  lastEpisodeDate: Object
+  lastEpisodeTimestamp: Object
+  lastEpisodeInfo: Object
+  availableEpisodes: Object
+  availableEpisodesDetail: Object
+  disqusIds: Object
+}
+
+type PageInfo {
+  hasNextPage: Boolean
+  nextPage: Int
+  prevPage: Int
+  total: Int
+  offset: Int
+  limit: Int
+  totalPages: Int
+  page: Int
+  hasPrevPage: Boolean
+}
+
+ */
